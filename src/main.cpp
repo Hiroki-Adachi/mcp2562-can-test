c016fb2b89772b729536ca94938ff9230131b3a9
Hiroki Adachi Macbook Pro <airobot317@gmail.com>
2024-08-05 12:28:40 +0900

    Make branch

--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,8 @@
 // const gpio_num_t kCanRx = GPIO_NUM_7;
 
 // sub
-const gpio_num_t kCanTx = GPIO_NUM_35;
-const gpio_num_t kCanRx = GPIO_NUM_36;
+const gpio_num_t kCanTx = GPIO_NUM_4;
+const gpio_num_t kCanRx = GPIO_NUM_5;
 
 void setup() {
   Serial.begin(115200);
